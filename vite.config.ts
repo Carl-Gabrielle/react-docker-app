33fc9a238881435c07df19f64341b85aa16437bd
carlgabrielle <carlgabrielleorfinada1@gmail.com>
2026-03-17 02:15:32 +0800

    updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: ['react-docker-app-1-za5z.onrender.com']
+  }
 })
